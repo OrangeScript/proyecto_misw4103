b7fc2883cce6b073cf2e678eda6c50e982f87cc5
Simón Buriticá <s.buritica@uniandes.edu.co>
2023-11-10 18:48:51 -0500

    Login y posts page objects

---
+++ kraken-node-scenarios/features/web/page_objects/LoginPage.js
@@ -0,0 +1,34 @@
+
+class LoginPage {
+  constructor(driver) {
+    this.driver = driver;
+    this.usernameInput = 'input[name="identification"]';
+    this.passwordInput = 'input[name="password"]';
+    this.submitButton = 'button[type="submit"]';
+  }
+
+  async open() {
+    
+    await this.driver.url('http://localhost:2368/ghost/#/signin');
+  }
+
+  async login(username, password) {
+    await this.open();
+
+    const usernameInput = await this.driver.$(this.usernameInput);
+    await usernameInput.setValue(username);
+
+    const passwordInput = await this.driver.$(this.passwordInput);
+    await passwordInput.setValue(password);
+
+    const submitButton = await this.driver.$(this.submitButton);
+    await submitButton.click();
+
+    await this.driver.waitUntil(async () => {
+      const currentUrl = await this.driver.getUrl();
+      return currentUrl.includes('dashboard');
+    });
+  }
+}
+
+module.exports = LoginPage;
--- kraken-node-scenarios/features/web/support/hooks.js
+++ kraken-node-scenarios/features/web/support/hooks.js
@@ -1,9 +1,14 @@
 const { After, Before } = require('@cucumber/cucumber');
 const { WebClient } = require('kraken-node');
+const LoginPage = require('../page_objects/LoginPage');
+const PostsPage = require('../page_objects/PostsPage');
 
 Before(async function() {
   this.deviceClient = new WebClient('chrome', {}, this.userId);
   this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
+
+  this.loginPage = new LoginPage(this.driver);
+  this.postsPage = new PostsPage(this.driver);
 })
 
 After(async function() {
