40a59899adc12c9af37588958464e296f3ce9b40
imdiegodev1 <da.naranjo10@uniandes.edu.co>
2023-11-10 20:39:23 -0500

    escenarios login en cypress

--- cypress-scenarios/cypress/e2e/login.cy.js
+++ cypress-scenarios/cypress/e2e/login.cy.js
@@ -1,14 +1,68 @@
+//import credentials from '../fixtures/credentials.json'
+
+const credentials = {
+    url: "http://localhost:2368/ghost/#/signin",
+    email: "hi@imdiego.dev",
+    password: "holamundo123",
+    badEmail: "bad@email.dev",
+    badPassword: "badPassword",
+    url_afteLoggin: "http://localhost:2368/ghost/#/dashboard" 
+}
+
 describe('Testing basic Login', () => {
     beforeEach(()=>{
-       cy.visit('http://localhost:2368/ghost/#/signin')
-        cy.wait(7000)
+       cy.visit(credentials.url)
+       cy.wait(7000)
+
+       //cy.fixture('credentials').then(function (data) {
+       // this.data = data;
+       //})
     })
-    it('Create an user and login', ()=>{
+    it('Login with correct credentials', ()=>{
         cy.get('form').within(() => {
-            cy.get('input.gh-input.email').type('Monitor')
-            cy.get('input.gh-input.password').type('Pruebas')
-            cy.get('button.login.gh-btn.gh-btn-login.gh-btn-block.gh-btn-icon.ember-view').click()
+            cy.fixture('index.json').then((locators) => {
+                cy.get(locators.email_input).type(credentials.email);
+                cy.get(locators.password_input).type(credentials.password);
+                cy.get(locators.login_button).click();
+                cy.wait(1000);
+                cy.url().should('eq', credentials.url_afteLoggin);
+            });
         })
-        cy.wait(1000) 
+    })
+
+    it('Login with bad username', ()=>{
+        cy.get('form').within(() => {
+            cy.fixture('index.json').then((locators) => {
+                cy.get(locators.email_input).type(credentials.badEmail);
+                cy.get(locators.password_input).type(credentials.password);
+                cy.get(locators.login_button).click();
+                cy.wait(1000);
+                cy.url().should('eq', credentials.url);
+            });
+        })
+    })
+
+    it('Login with bad password', ()=>{
+        cy.get('form').within(() => {
+            cy.fixture('index.json').then((locators) => {
+                cy.get(locators.email_input).type(credentials.email);
+                cy.get(locators.password_input).type(credentials.badPassword);
+                cy.get(locators.login_button).click();
+                cy.wait(1000);
+                cy.url().should('eq', credentials.url);
+            });
+        })
+    })
+
+    it('Login with bad credentials', ()=>{
+        cy.get('form').within(() => {
+            cy.fixture('index.json').then((locators) => {
+                cy.get(locators.email_input).type(credentials.badEmail);
+                cy.get(locators.password_input).type(credentials.badPassword);
+                cy.get(locators.login_button).click();
+                cy.wait(1000);
+                cy.url().should('eq', credentials.url);
+            });
+        })
     })
   })
