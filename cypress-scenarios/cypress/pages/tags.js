99528d29881aac54026e37bf13762e043fbc48c3
imdiegodev1 <da.naranjo10@uniandes.edu.co>
2023-11-16 01:54:23 -0500

    crear tag sin nombre, crar tag interno, editar un tag, eliminar un tag

--- cypress-scenarios/cypress/pages/tags.js
+++ cypress-scenarios/cypress/pages/tags.js
@@ -9,11 +9,24 @@ class Tag {
     verifyTagsPage(tags_url)
     {
         cy.url().should('contain', tags_url)
+        cy.screenshot()
     }
 
     writeDate(locator, text)
     {
         cy.get(locator).type(text)
+    }
+
+    verifyContennt(text)
+    {
+        cy.contains(text)
+        cy.screenshot()
+    }
+
+    getFirstTag(locator)
+    {
+        cy.get(locator).first().click()
+        cy.screenshot()
     }
 
 }
