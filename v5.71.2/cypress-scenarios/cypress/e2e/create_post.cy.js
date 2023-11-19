2a714aff1e355adee0f2abdba13aeed3d338ae8a
Simón Buriticá <s.buritica@uniandes.edu.co>
2023-11-19 17:29:52 -0500

    Screenshots cypress ghost 5_71

--- v5.71.2/cypress-scenarios/cypress/e2e/create_post.cy.js
+++ v5.71.2/cypress-scenarios/cypress/e2e/create_post.cy.js
@@ -32,44 +32,59 @@ describe("Testing create post", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get('a[data-test-nav="new-story"]').click();
+    cy.screenshot();
     cy.get("textarea[data-test-editor-title-input]").clear();
+    cy.screenshot();
     cy.get('p[data-koenig-dnd-droppable="true"]').clear();
+    cy.screenshot();
     cy.get("button[class='gh-publish-trigger']").should('not.exist');
+    cy.screenshot();
   });
 
   it("should create a post with a button", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get('a[data-test-nav="new-story"]').click();
+    cy.screenshot();
     cy.get("textarea[data-test-editor-title-input]").type("Post with button");
+    cy.screenshot();
     cy.wait(2000);
     cy.get('p[data-koenig-dnd-droppable="true"]').click();
     cy.wait(2000);
     cy.get('button[aria-label="Add a card"]').click();
+    cy.screenshot();
     cy.wait(1000);
     cy.get('button[data-kg-card-menu-item="Button"]').click();
     cy.get('input[data-testid="button-input-text"]').click().type("Button");
+    cy.screenshot();
     cy.get('input[data-testid="button-input-url"]')
       .click()
       .type("https://www.coursera.org/");
+    cy.screenshot();
     cy.get('input[data-testid="button-input-url"]').type("{enter}");
     cy.wait(1000);
     cy.get(".gh-publish-trigger").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-cta > button").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get('button[data-test-button="confirm-publish"]').click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-title").should("exist");
+    cy.screenshot();
   });
 
   it("should create a post with HTML content", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get('a[data-test-nav="new-story"]').click();
+    cy.screenshot();
     cy.get("textarea[data-test-editor-title-input]").type(
       "Post with HTML content"
     );
+    cy.screenshot();
     cy.wait(2000);
     cy.get('p[data-koenig-dnd-droppable="true"]').click();
     cy.wait(2000);
@@ -79,68 +94,89 @@ describe("Testing create post", () => {
     cy.get('div[data-kg-card-editing="true"]')
       .click()
       .type("<h1>Hello, HTML!</h1>");
+    cy.screenshot();
     cy.get('div[data-kg-card-editing="true"]').type("{enter}");
     cy.wait(1000);
     cy.get(".gh-publish-trigger").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-cta > button").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get('button[data-test-button="confirm-publish"]').click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-title").should("exist");
+    cy.screenshot();
   });
 
   it("should create a post with Markdown content", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get('a[data-test-nav="new-story"]').click();
+    cy.screenshot();
     cy.get("textarea[data-test-editor-title-input]").type(
       "Post with Markdown content"
     );
+    cy.screenshot();
     cy.wait(2000);
     cy.get('p[data-koenig-dnd-droppable="true"]').click();
     cy.wait(2000);
     cy.get('button[aria-label="Add a card"]').click();
     cy.wait(1000);
+    cy.screenshot();
     cy.get('button[data-kg-card-menu-item="HTML"]').click();
     cy.get('div[data-kg-card-editing="true"]')
       .click()
       .type("# Hello, Markdown!");
+    cy.screenshot();
     cy.get('div[data-kg-card-editing="true"]').type("{enter}");
     cy.wait(1000);
     cy.get(".gh-publish-trigger").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-cta > button").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get('button[data-test-button="confirm-publish"]').click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-title").should("exist");
+    cy.screenshot();
   });
 
   it('should create a post using "/" shortcut for add HTML content', async () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get('a[data-test-nav="new-story"]').click();
+    cy.screenshot();
     cy.get("textarea[data-test-editor-title-input]").type(
       "Post with '/' shortcut"
     );
+    cy.screenshot();
     cy.wait(2000);
     cy.get('p[data-koenig-dnd-droppable=true]').click();
     cy.wait(2000);
     cy.get('p[data-koenig-dnd-droppable=true]').type("/");
+    cy.screenshot();
     cy.wait(2000);
     cy.get('button[data-kg-card-menu-item="HTML"]').click();
     cy.get('div[data-kg-card-editing="true"]')
       .click()
       .type("<h1>Hello, '/' shortcut HTML!</h1>");
+    cy.screenshot(); 
     cy.get('div[data-kg-card-editing="true"]').type("{enter}");
     cy.wait(1000);
     cy.get(".gh-publish-trigger").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-cta > button").click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get('button[data-test-button="confirm-publish"]').click();
+    cy.screenshot();
     cy.wait(2000);
     cy.get(".gh-publish-title").should("exist");
+    cy.screenshot();
   });
 });
--- v5.71.2/cypress-scenarios/cypress/pages/login.js
+++ v5.71.2/cypress-scenarios/cypress/pages/login.js
@@ -4,15 +4,17 @@ class Login
     setUserName(locator, username)
     {
         cy.get(locator).type(username)
+        cy.screenshot()
     }
 
     setPassword(locator, username)
     {
         cy.get(locator).type(username)
+        cy.screenshot()
     }
 
     clickLogin(locator) {
-        cy.get(locator).click()
+        cy.get(locator).click()   
     }
 
     verifyLogin(dashboard_url) {
